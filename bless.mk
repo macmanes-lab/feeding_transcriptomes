356fd6b0b6c630b9c8845449f5ccab57ce5980d6
Matt MacManes <macmanes@gmail.com>
2014-10-19 09:16:10 -0400

    slim hard coded path

--- bless.mk
+++ bless.mk
@@ -7,72 +7,67 @@
 ############################################
 
 
-all:10M.1.corrected.fastq.gz 10M.2.corrected.fastq.gz
-20M.1.corrected.fastq.gz 20M.2.corrected.fastq.gz \
-50M.1.corrected.fastq.gz 50M.2.corrected.fastq.gz \
-75M.1.corrected.fastq.gz 75M.2.corrected.fastq.gz \
-100M.1.corrected.fastq.gz 100M.2.corrected.fastq.gz
+all:$(CURDIR)/10M_corr/10M.1.corrected.fastq.gz $(CURDIR)/10M_corr/10M.2.corrected.fastq.gz
+$(CURDIR)/20M_corr/20M.1.corrected.fastq.gz $(CURDIR)/20M_corr/20M.2.corrected.fastq.gz \
+$(CURDIR)/50M_corr/50M.1.corrected.fastq.gz $(CURDIR)/50M_corr/50M.2.corrected.fastq.gz \
+$(CURDIR)/75M_corr/75M.1.corrected.fastq.gz $(CURDIR)/75M_corr/75M.2.corrected.fastq.gz \
+$(CURDIR)/100M_corr/100M.1.corrected.fastq.gz $(CURDIR)/100M_corr/100M.2.corrected.fastq.gz
 
 
 K=19
 
 samp := 10M
-$(samp).1.corrected.fastq.gz $(samp).2.corrected.fastq.gz:raw.$(samp).SRR797058_1.fastq raw.$(samp).SRR797058_2.fastq
+$(CURDIR)/$(samp)_corr/$(samp).1.corrected.fastq.gz $(CURDIR)/$(samp)_corr/$(samp).2.corrected.fastq.gz:\
+$(CURDIR)/$(samp)_corr/raw.$(samp).SRR797058_1.fastq $(CURDIR)/$(samp)_corr/raw.$(samp).SRR797058_2.fastq
 	@echo '\n\n'BEGIN ERROR CORRECTION: `date +'%a %d%b%Y  %H:%M:%S'`
 	@echo Results will be in a file named *corrected.fastq.gz
 	@echo Settings used: bless kmerlength = $(K)
-	mkdir -p /mnt/data3/macmanes/subsamp/$(samp)_corr
-	cd /mnt/data3/macmanes/subsamp/$(samp)_corr
-	bless -kmerlength $(K) -read1 $< -read2 $(word 2,$^) -verify -notrim -prefix $(samp)
-	gzip $(samp).1.corrected.fastq $(samp).2.corrected.fastq &
+	bless -kmerlength $(K) -read1 $< -read2 $(word 2,$^) -verify -notrim -prefix $(CURDIR)/$(samp)_corr/$(samp)
+	gzip $(CURDIR)/$(samp)_corr/$(samp).1.corrected.fastq $(CURDIR)/$(samp)_corr/$(samp).2.corrected.fastq &
 
 samp := 20M
-$(samp).1.corrected.fastq.gz $(samp).2.corrected.fastq.gz:raw.$(samp).SRR797058_1.fastq raw.$(samp).SRR797058_2.fastq
+$(CURDIR)/$(samp)_corr/$(samp).1.corrected.fastq.gz $(CURDIR)/$(samp)_corr/$(samp).2.corrected.fastq.gz:\
+$(CURDIR)/$(samp)_corr/raw.$(samp).SRR797058_1.fastq $(CURDIR)/$(samp)_corr/raw.$(samp).SRR797058_2.fastq
 	@echo '\n\n'BEGIN ERROR CORRECTION: `date +'%a %d%b%Y  %H:%M:%S'`
 	@echo Results will be in a file named *corrected.fastq.gz
 	@echo Settings used: bless kmerlength = $(K)
-	mkdir -p /mnt/data3/macmanes/subsamp/$(samp)_corr
-	cd /mnt/data3/macmanes/subsamp/$(samp)_corr
-	bless -kmerlength $(K) -read1 $< -read2 $(word 2,$^) -verify -notrim -prefix $(samp)
-	gzip $(samp).1.corrected.fastq $(samp).2.corrected.fastq &
+	bless -kmerlength $(K) -read1 $< -read2 $(word 2,$^) -verify -notrim -prefix $(CURDIR)/$(samp)_corr/$(samp)
+	gzip $(CURDIR)/$(samp)_corr/$(samp).1.corrected.fastq $(CURDIR)/$(samp)_corr/$(samp).2.corrected.fastq &
 
 samp := 50M
-$(samp).1.corrected.fastq.gz $(samp).2.corrected.fastq.gz:raw.$(samp).SRR797058_1.fastq raw.$(samp).SRR797058_2.fastq
+$(CURDIR)/$(samp)_corr/$(samp).1.corrected.fastq.gz $(CURDIR)/$(samp)_corr/$(samp).2.corrected.fastq.gz:\
+$(CURDIR)/$(samp)_corr/raw.$(samp).SRR797058_1.fastq $(CURDIR)/$(samp)_corr/raw.$(samp).SRR797058_2.fastq
 	@echo '\n\n'BEGIN ERROR CORRECTION: `date +'%a %d%b%Y  %H:%M:%S'`
 	@echo Results will be in a file named *corrected.fastq.gz
 	@echo Settings used: bless kmerlength = $(K)
-	mkdir -p /mnt/data3/macmanes/subsamp/$(samp)_corr
-	cd /mnt/data3/macmanes/subsamp/$(samp)_corr
-	bless -kmerlength $(K) -read1 $< -read2 $(word 2,$^) -verify -notrim -prefix $(samp)
-	gzip $(samp).1.corrected.fastq $(samp).2.corrected.fastq &
+	bless -kmerlength $(K) -read1 $< -read2 $(word 2,$^) -verify -notrim -prefix $(CURDIR)/$(samp)_corr/$(samp)
+	gzip $(CURDIR)/$(samp)_corr/$(samp).1.corrected.fastq $(CURDIR)/$(samp)_corr/$(samp).2.corrected.fastq &
 
 samp := 75M
-$(samp).1.corrected.fastq.gz $(samp).2.corrected.fastq.gz:raw.$(samp).SRR797058_1.fastq raw.$(samp).SRR797058_2.fastq
+$(CURDIR)/$(samp)_corr/$(samp).1.corrected.fastq.gz $(CURDIR)/$(samp)_corr/$(samp).2.corrected.fastq.gz:\
+$(CURDIR)/$(samp)_corr/raw.$(samp).SRR797058_1.fastq $(CURDIR)/$(samp)_corr/raw.$(samp).SRR797058_2.fastq
 	@echo '\n\n'BEGIN ERROR CORRECTION: `date +'%a %d%b%Y  %H:%M:%S'`
 	@echo Results will be in a file named *corrected.fastq.gz
 	@echo Settings used: bless kmerlength = $(K)
-	mkdir -p /mnt/data3/macmanes/subsamp/$(samp)_corr
-	cd /mnt/data3/macmanes/subsamp/$(samp)_corr
-	bless -kmerlength $(K) -read1 $< -read2 $(word 2,$^) -verify -notrim -prefix $(samp)
-	gzip $(samp).1.corrected.fastq $(samp).2.corrected.fastq &
+	bless -kmerlength $(K) -read1 $< -read2 $(word 2,$^) -verify -notrim -prefix $(CURDIR)/$(samp)_corr/$(samp)
+	gzip $(CURDIR)/$(samp)_corr/$(samp).1.corrected.fastq $(CURDIR)/$(samp)_corr/$(samp).2.corrected.fastq &
 
 samp := 100M
-$(samp).1.corrected.fastq.gz $(samp).2.corrected.fastq.gz:raw.$(samp).SRR797058_1.fastq raw.$(samp).SRR797058_2.fastq
+$(CURDIR)/$(samp)_corr/$(samp).1.corrected.fastq.gz $(CURDIR)/$(samp)_corr/$(samp).2.corrected.fastq.gz:\
+$(CURDIR)/$(samp)_corr/raw.$(samp).SRR797058_1.fastq $(CURDIR)/$(samp)_corr/raw.$(samp).SRR797058_2.fastq
 	@echo '\n\n'BEGIN ERROR CORRECTION: `date +'%a %d%b%Y  %H:%M:%S'`
 	@echo Results will be in a file named *corrected.fastq.gz
 	@echo Settings used: bless kmerlength = $(K)
-	mkdir -p /mnt/data3/macmanes/subsamp/$(samp)_corr
-	cd /mnt/data3/macmanes/subsamp/$(samp)_corr
-	bless -kmerlength $(K) -read1 $< -read2 $(word 2,$^) -verify -notrim -prefix $(samp)
-	gzip $(samp).1.corrected.fastq $(samp).2.corrected.fastq &
+	bless -kmerlength $(K) -read1 $< -read2 $(word 2,$^) -verify -notrim -prefix $(CURDIR)/$(samp)_corr/$(samp)
+	gzip $(CURDIR)/$(samp)_corr/$(samp).1.corrected.fastq $(CURDIR)/$(samp)_corr/$(samp).2.corrected.fastq &
 
-samp := 10M
-Trinity.fasta:$(samp)_corrk19.1.corrected.fastq.gz $(samp)_corrk19.2.corrected.fastq.gz
-	Trinity --seqType fq --JM 50G --trimmomatic \
-    --left $<  \
-    --right $(word 2,$^) \
-    --CPU 24 --output $(samp) \
-    --quality_trimming_params "ILLUMINACLIP:/mnt/data3/macmanes/subsamp/barcodes.fa:2:40:15 LEADING:2 TRAILING:2 MINLEN:25"
+#samp := 10M
+#Trinity.fasta:$(samp)_corrk19.1.corrected.fastq.gz $(samp)_corrk19.2.corrected.fastq.gz
+#	Trinity --seqType fq --JM 50G --trimmomatic \
+#    --left $<  \
+#    --right $(word 2,$^) \
+#    --CPU 24 --output $(samp) \
+#    --quality_trimming_params "ILLUMINACLIP:/mnt/data3/macmanes/subsamp/barcodes.fa:2:40:15 LEADING:2 TRAILING:2 MINLEN:25"
 
 
 
