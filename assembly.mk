86744ad1ed850480c5ef2788c5103daae8d5dfa9
Matt MacManes <matthew.macmanes@unh.edu>
2014-11-17 08:30:08 -0500

    typo's

--- assembly.mk
+++ assembly.mk
@@ -6,16 +6,17 @@
 ###
 ############################################
 
+samp20 := 20M
+samp100 := 100M
 
 all:\
-$(CURDIR)/$(samp20)_corr/$(samp20).ec.P2/Trinity.fasta
-$(CURDIR)/$(samp100)_corr/$(samp100).ec.P2/Trinity.fasta
+$(CURDIR)/$(samp20)_corr/$(samp20).ec.P2/Trinity.fasta \
+$(CURDIR)/$(samp100)_corr/$(samp100).ec.P2/Trinity.fasta \
 $(CURDIR)/20M.ec.P2.C50/Trinity.fasta \
 $(CURDIR)/100M.ec.P2.C50/Trinity.fasta
 
 CPU=32
 
-samp20 := 20M
 $(CURDIR)/$(samp20)_corr/$(samp20).ec.P2/Trinity.fasta:\
 $(CURDIR)/$(samp20)_corr/$(samp20)_corrk19.1.corrected.fastq.gz $(CURDIR)/$(samp20)_corr/$(samp20)_corrk19.2.corrected.fastq.gz
 	Trinity --seqType fq --JM 50G --trimmomatic \
@@ -24,7 +25,6 @@ $(CURDIR)/$(samp20)_corr/$(samp20)_corrk19.1.corrected.fastq.gz $(CURDIR)/$(samp
     --CPU $(CPU) --output $(samp20).ec.P2 \
     --quality_trimming_params "ILLUMINACLIP:$(CURDIR)/barcodes.fa:2:40:15 LEADING:2 TRAILING:2 MINLEN:25"
 
-samp100 := 100M
 $(CURDIR)/$(samp100)_corr/$(samp100).ec.P2/Trinity.fasta:\
 $(CURDIR)/$(samp100)_corr/$(samp100)_corrk19.1.corrected.fastq.gz $(CURDIR)/$(samp100)_corr/$(samp100)_corrk19.2.corrected.fastq.gz
 	Trinity --seqType fq --JM 50G --trimmomatic \
